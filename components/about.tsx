ba03c715a6f939f38d3d22c14a761c6db82b07c0
Mani Sreekar Palakurthy <manisreekarpalakurthy@Manis-MacBook-Air.local>
2024-03-18 17:02:10 -0700

    implemented about section

---
+++ components/about.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import SectionHeading from "./section-heading";
+
+const About = () => {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+
+      <p className="mb-3">
+        As a determined software development engineer, I possess a proven
+        proficiency in problem-solving and an unwavering passion for technology.
+        With a{" "}
+        <span className="font-medium">
+          Master's in Computer Science from Arizona State University{" "}
+        </span>{" "}
+        and hands-on experience in industry-leading tools and technologies,
+        including <span className="font-medium">MERN Stack, </span> I am
+        equipped to tackle diverse challenges in software development.
+      </p>
+      <p className="mb-3">
+        <span className="italic">
+          From designing efficient strategies to mitigate fraud risks to
+          developing dynamic projects, I thrive in different environments.
+        </span>
+      </p>
+      <p className="mb-3">
+        Proficient in the <span className="font-medium">SDLC</span>, from
+        requirements analysis to deployment and maintenance. Complemented by a
+        knack for continuous learning, I am eager to contribute my skills to
+        come up with impactful solutions in software engineering roles.
+      </p>
+    </motion.section>
+  );
+};
+
+export default About;
---
+++ components/projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+
+const Projects = () => {
+  return (
+    <>
+      <SectionHeading>My Projects</SectionHeading>
+    </>
+  );
+};
+
+export default Projects;
